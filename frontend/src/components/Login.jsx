0ea3c5c2ddf66082946f8029f3aabfcd1545900b
AsyaKnyazeva <knyazeva478@gmail.com>
2022-11-07 12:18:11 +0300

    step2 environ

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -15,7 +15,7 @@ const LoginSchema = yup.object().shape({
 const Login = () => (
   <div className="container-fluid h-100">
     <div className="row justify-content-center align-content-center h-100">
-      <div className="col-12 col-md-8 col-xxl-6">
+      <div className="col-10 col-md-8 col-xxl-6">
         <div className="card shadow-sm">
           <div className="card-body row p-5">
             <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
@@ -28,7 +28,7 @@ const Login = () => (
               }}
               validationSchema={LoginSchema}
               onSubmit={(values, { resetForm }) => {
-                alert(JSON.stringify(values, null, 2));
+                console.log(JSON.stringify(values, null, 2));
                 resetForm();
               }}
             >
