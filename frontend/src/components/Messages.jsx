2165c86e2c176242eb98d6acc4243c655459d8d2
AsyaKnyazeva <knyazeva478@gmail.com>
2022-11-21 20:12:26 +0300

    fix msgPage

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -17,10 +17,19 @@ const Messages = () => {
   const activeChanel = useSelector((state) => channelsSelectors
     .selectById(state, currentChannelId)); 
   const currentMessages = messages.filter((m) => m.chanelId === currentChannelId);
+
   const chat = useContext(ApiContext);
   const auth = useContext(AuthContext);
+
   const inputRef = useRef();
+  const lastMessageRef = useRef();
+  
   useEffect(() => inputRef.current.focus());
+  useEffect(() => {
+    lastMessageRef.current.scrollIntoView({
+      behavior: 'smooth',
+    });
+  }, [currentMessages]);
 
   const validationSchema = yup.object().shape({
     body: yup.string()
@@ -42,6 +51,7 @@ const Messages = () => {
             <div className="text-break mb-2" key={m.id}><b>{m.username}</b>: {m.body}
             </div>
                       ))}
+                      <span ref={lastMessageRef} />
                       </div>
         <div className="mt-auto px-5 py-3">
           <Formik
