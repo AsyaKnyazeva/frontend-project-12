781ad466402e11e9730ba35a3f42662760e235fd
AsyaKnyazeva <knyazeva478@gmail.com>
2022-11-28 16:47:02 +0300

    fixed bootstrap import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { io } from 'socket.io-client';
 import App from './App.jsx';
 import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/scss/bootstrap.scss';
 
 const socket = io();
 const vdom = App(socket);
